cf76fbcb36a58746af95354b0a02b326510ef4c7
Dmytro9513 <Goncharenko9513@gmail.com>
2023-08-05 19:23:29 +0200

    9

---
+++ HW_9/HW_9.py
@@ -0,0 +1,69 @@
+# Функція обробки помилок введення
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError):
+            return "Invalid input. Please try again."
+
+    return inner
+
+# Додамо словник для зберігання контактів
+contacts = {}
+
+# Функція додавання контакту
+@input_error
+def add_contact(command):
+    _, name, phone = command.split()
+    contacts[name] = phone
+    return f"Contact '{name}' with phone '{phone}' has been added."
+
+# Функція зміни номера телефону контакту
+@input_error
+def change_phone(command):
+    _, name, phone = command.split()
+    contacts[name] = phone
+    return f"Phone number for contact '{name}' has been changed to '{phone}'."
+
+# Функція виведення номера телефону контакту
+@input_error
+def show_phone(command):
+    _, name = command.split()
+    return f"Phone number for contact '{name}': {contacts[name]}"
+
+# Функція виведення всіх контактів
+def show_all_contacts():
+    if not contacts:
+        return "No contacts found."
+    result = "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    return result
+
+# Головна функція бота
+def main():
+    print("How can I help you?")
+
+    while True:
+        command = input("> ").lower()
+
+        if command == "good bye" or command == "close" or command == "exit":
+            print("Good bye!")
+            break
+        elif command == "hello" or command == "hi" or command == "hallo":
+            print("How can I help you?")
+        elif command.startswith("add"):
+            response = add_contact(command)
+            print(response)
+        elif command.startswith("change"):
+            response = change_phone(command)
+            print(response)
+        elif command.startswith("phone"):
+            response = show_phone(command)
+            print(response)
+        elif command == "show all":
+            response = show_all_contacts()
+            print(response)
+        else:
+            print("Unknown command. Please try again.")
+
+if __name__ == "__main__":
+    main()
